3006b16bdff78dc867648bd654a0cf0a38ec9e0e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-29 12:30:51 -0500

    add functioality

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 //import styles
 import GlobalStyle from '../../styles/GlobalStyles';
+import { Container } from './styles';
 //import form
 import TodoForm from '../TodoForm';
 //import todolist
@@ -13,13 +14,21 @@ export const App = () => {
 	const [todos, setTodos] = useState([]);
 
 	return (
-		<>
+		<Container>
 			<GlobalStyle />
 			<Typography component='h1' variant='h2'>
 				Todos
 			</Typography>
-			<TodoForm saveTodo={console.warn} />
+			<TodoForm
+				saveTodo={(todoText) => {
+					const trimmedText = todoText.trim();
+
+					if (trimmedText.length > 0) {
+						setTodos([...todos, trimmedText]);
+					}
+				}}
+			/>
 			<TodoList todos={todos} />
-		</>
+		</Container>
 	);
 };
