ef9ddbfb94cb317f166889b3ba8522e58ebcc96e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-29 13:00:40 -0500

    Updates

---
+++ precache-manifest.36d491e851e075aa6b66462624c43b1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6feb0dbe6124fd4067f225554ab2e814",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "73a963189c97e2653a34",
+    "url": "/todo-app/static/js/2.5011e0e9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-app/static/js/2.5011e0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85cfe78869cd24bfca56",
+    "url": "/todo-app/static/js/main.cfa422ad.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
